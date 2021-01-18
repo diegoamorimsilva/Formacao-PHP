adc759b5a4d423cd943fd84916395ea960f5769c
diegoamorimsilva <47456902+diegoamorimsilva@users.noreply.github.com>
2021-01-18 11:03:23 -0300

    Funçao para realizar saques na conta

---
+++ avancando-com-PHP/sacarDaConta.php
@@ -0,0 +1,34 @@
+<?php
+
+function sacar($conta, $valor){
+
+    if($conta['saldo'] < $valor) {
+        echo "Você não pode sacar";
+    }else{
+        $conta['saldo'] -= 500;
+    }
+    return $conta;
+}
+
+$conta1 = [
+    'titular' => 'Diego',
+    'saldo' => 2000
+];
+$conta2 = [
+    'titular' => 'Maria',
+    'saldo' => 8000
+];
+$conta3 = [
+    'titular' => 'Joana',
+    'saldo' => 300
+];
+
+$contas = [$conta1, $conta2, $conta3];
+
+$contas[0] = sacar($contas[0] , 500);
+
+for ($i=0; $i < count($contas) ; $i++) { 
+    echo $contas[$i]['titular'];
+    echo ' ';
+    echo $contas[$i]['saldo'].PHP_EOL;
+}
