8b1a64348a936dfa0517d77849bdc9a8a3b92ebf
diegoamorimsilva <47456902+diegoamorimsilva@users.noreply.github.com>
2021-01-18 11:03:00 -0300

    melhorando fução de exibição

--- avancando-com-PHP/mundofuncao.php
+++ avancando-com-PHP/mundofuncao.php
@@ -1,7 +1,24 @@
 <?php
 
-function exibeOla() {
-    echo "Ola Mundo".PHP_EOL;
-}
+function exibeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+} 
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.689-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
+];
 
-exibeOla();
+foreach ($contasCorrentes as $cpf => $conta) {
+    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+}
