3b1093d99bd2de8ee3b2df82b169d0a8fcb7edbc
diegoamorimsilva <47456902+diegoamorimsilva@users.noreply.github.com>
2021-01-19 09:29:19 -0300

    Adicio0nando HTML

--- avancando-com-PHP/sacarDaConta.php
+++ avancando-com-PHP/sacarDaConta.php
@@ -32,3 +32,28 @@ for ($i=0; $i < count($contas) ; $i++) {
     echo ' ';
     echo $contas[$i]['saldo'].PHP_EOL;
 }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+
+    <dl>
+        <?php foreach($contas as  $conta) { ?>
+        <dt>
+            <h3><?= $conta['titular']; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
