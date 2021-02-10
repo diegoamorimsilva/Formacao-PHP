7eb109bba01e401236e4c4b59b9ea720b8322968
Diego Amorim <diego008dover@gmail.com>
2021-02-10 16:18:50 -0300

    automatizando execução de scripts

--- PHP Composer Dependências Autoload e Publicação/src/Buscador.php
+++ PHP Composer Dependências Autoload e Publicação/src/Buscador.php
@@ -29,7 +29,7 @@ class Buscador
         $html = $resposta->getBody();
         $this->crawler->addHtmlContent($html);
 
-        $elementosCursos = $this->crawler->filter('h3.card-title');
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
         $cursos = [];
 
         foreach ($elementosCursos as $elemento) {
